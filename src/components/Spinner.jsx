a43b6fa7f32906845d0c82fefbfccfb15221cc7c
micke <mickeyfowose@gmail.com>
2025-09-10 03:17:14 +0100

    initial commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,6 @@
+
+import { Spinner as FlowbiteSpinner } from "flowbite-react";
+
+export default function Spinner() {
+  return <FlowbiteSpinner aria-label="Purple spinner example"color="purple" />;
+}
